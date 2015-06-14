ead7b60abcc5debf23fbeea62aad11c222244af3
J. Ryan Rembert <j.ryan.rembert@gmail.com>
2015-06-14 14:22:03 -0700

    Remove old playground, add new

---
+++ PlaylistBrowserSwift/algorhythms-playground.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var str = "Hello, playground"
+
+// Exclusive range
+for i in 0..<5 {
+    println(i)
+}
+
+// Inclusive range
+for i in 0...5 {
+    println(i)
+}
